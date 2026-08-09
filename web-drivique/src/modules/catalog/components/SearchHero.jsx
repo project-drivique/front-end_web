0ee6b4519b4fcf24c827c7eb2d03a26178ca2b03
vanessaperdomo <p.laurav2@gmail.com>
2026-08-09 14:39:19 -0500

    actualice el diseño del catalogo

--- web-drivique/src/modules/catalog/components/SearchHero.jsx
+++ web-drivique/src/modules/catalog/components/SearchHero.jsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next'
 import { FaMapMarkerAlt, FaCalendarAlt } from 'react-icons/fa'
 
 import { SUCURSALES, CIUDADES } from '../constants'
-import AlertaModal from './AlertModal'
+import AlertaModal from './AlertaModal'
 
 export default function HeroBusqueda({
   c,
