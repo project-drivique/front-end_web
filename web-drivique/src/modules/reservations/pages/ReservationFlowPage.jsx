2dd11fd1ed4e4786e8661e8064e0b63fd0fc636e
vanessaperdomo <p.laurav2@gmail.com>
2026-08-20 10:31:57 -0500

    fix(reservations): import CatalogPage.css for zoom consistency

--- web-drivique/src/modules/reservations/pages/ReservationFlowPage.jsx
+++ web-drivique/src/modules/reservations/pages/ReservationFlowPage.jsx
@@ -20,6 +20,8 @@ import PersonalData from '../components/PersonalData'
 import VehicleDetailsModal from '../../catalog/components/detail/VehicleDetailsModal'
 import ContractSignature from '../../contracts/components/ContractSignature'
 
+import '../../catalog/pages/CatalogPage.css'
+
 const IcoArrow = () => (
   <svg width="16" height="16" fill="none" stroke="currentColor" strokeWidth="2.5" viewBox="0 0 24 24">
     <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
